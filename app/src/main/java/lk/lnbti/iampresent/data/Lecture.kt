1a4f509d3a5304df3953d0e4683fbe5386865229
udenisilva <ukui.silva@gmail.com>
2023-11-19 00:26:54 +0530

    update data class

--- app/src/main/java/lk/lnbti/iampresent/data/Lecture.kt
+++ app/src/main/java/lk/lnbti/iampresent/data/Lecture.kt
@@ -3,35 +3,57 @@ package lk.lnbti.iampresent.data
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
-import java.io.Serializable
-import java.sql.Date
-import java.sql.Time
 
+/**
+ * Parcelable data class representing a list of lectures.
+ *
+ * @property items List of [Lecture] objects.
+ */
 @Parcelize
 data class LectureList(
-   // @SerializedName("lecturestatusid") val lecturestatusid: Int,
-   // @SerializedName("statusname") val statusname: String?=null,
-    val items:List<Lecture>
+    val items: List<Lecture>
 ) : Parcelable
 
+/**
+ * Parcelable data class representing the status of a lecture.
+ *
+ * @property lectureStatusId Unique identifier for the lecture status.
+ * @property statusName Name of the lecture status.
+ */
 @Parcelize
 data class LectureStatus(
-    @SerializedName("lecturestatusid") val lectureStatusId: Int?=0,
-    @SerializedName("statusname") val statusName: String=""
-) : Parcelable
-@Parcelize
-data class Lecture(
-    @SerializedName("lectureid") val lectureId: Int=0,
-    @SerializedName("venue") val location: String,
-    @SerializedName("startdate") val startDate: String="",
-    @SerializedName("starttime") val startTime: String="",
-    @SerializedName("enddate") val endDate: String="",
-    @SerializedName("endtime") val endTime: String="",
-    @SerializedName("semester") val semester: Int=0,
-    @SerializedName("subject") val subject: String,
-    @SerializedName("batch") val batch: String,
-    @SerializedName("organizerid") val organizer: User=User(),
-    @SerializedName("lecturerid") val lecturer: User=User(),
-    @SerializedName("lecturestatusid") val lectureStatus: LectureStatus=LectureStatus()
+    @SerializedName("lecturestatusid") val lectureStatusId: Int? = 0,
+    @SerializedName("statusname") val statusName: String = ""
 ) : Parcelable
 
+/**
+ * Parcelable data class representing a lecture.
+ *
+ * @property lectureId Unique identifier for the lecture.
+ * @property location location of the lecture.
+ * @property startDate Start date of the lecture in the format "yyyy-MM-dd".
+ * @property startTime Start time of the lecture in the format "HH:mm:ss".
+ * @property endDate End date of the lecture in the format "yyyy-MM-dd".
+ * @property endTime End time of the lecture in the format "HH:mm:ss".
+ * @property semester Semester of the lecture.
+ * @property subject Subject of the lecture.
+ * @property batch Batch associated with the lecture.
+ * @property organizer Organizer of the lecture, represented by a [User] object.
+ * @property lecturer Lecturer of the lecture, represented by a [User] object.
+ * @property lectureStatus Status of the lecture, represented by a [LectureStatus] object.
+ */
+@Parcelize
+data class Lecture(
+    @SerializedName("lectureid") val lectureId: Long = 0,
+    @SerializedName("venue") val location: String,
+    @SerializedName("startdate") val startDate: String = "0000-00-00",
+    @SerializedName("starttime") val startTime: String = "00:00:00",
+    @SerializedName("enddate") val endDate: String = "0000-00-00",
+    @SerializedName("endtime") val endTime: String = "00:00:00",
+    @SerializedName("semester") val semester: Int = 0,
+    @SerializedName("subject") val subject: String,
+    @SerializedName("batch") val batch: String,
+    @SerializedName("organizerid") val organizer: User = User(),
+    @SerializedName("lecturerid") val lecturer: User = User(),
+    @SerializedName("lecturestatusid") val lectureStatus: LectureStatus = LectureStatus()
+) : Parcelable
--- app/src/main/java/lk/lnbti/iampresent/data/User.kt
+++ app/src/main/java/lk/lnbti/iampresent/data/User.kt
@@ -4,18 +4,32 @@ import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
+/**
+ * Data class representing a user role.
+ *
+ * @property roleid The unique identifier for the role.
+ * @property name The name of the role.
+ */
 @Parcelize
 data class Role(
     @SerializedName("roleid") val roleid: Int,
-    @SerializedName("name") val name: String?=null
+    @SerializedName("name") val name: String? = null
 ) : Parcelable
 
+/**
+ * Data class representing a user.
+ *
+ * @property userid The unique identifier for the user.
+ * @property name The name of the user.
+ * @property email The email address of the user.
+ * @property roleid The role assigned to the user.
+ * @property isuseravailable An indicator of user availability(active/deactivate).
+ */
 @Parcelize
 data class User(
-    @SerializedName("userid") val userid: Int?=null,
-    @SerializedName("name") val name: String="",
-    @SerializedName("email") val email: String?=null,
-    @SerializedName("token") val token: String?=null,
-    @SerializedName("roleid") val roleid: Role?=null,
-    @SerializedName("isuseravailable") val isuseravailable: Int?=null
+    @SerializedName("userid") val userid: Int? = null,
+    @SerializedName("name") val name: String = "",
+    @SerializedName("email") val email: String,
+    @SerializedName("roleid") val roleid: Role? = null,
+    @SerializedName("isuseravailable") val isuseravailable: Int? = null
 ) : Parcelable
